a798d3a1187baa70378b4cb8b994064657f2bd7e
SJC-s <sssm4719@gmail.com>
2024-10-01 16:46:26 +0900

    #3 예약프로젝트 nav 메뉴 버튼 클릭에 따라 UI 변경확인

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import './App.css'
-import {BrowserRouter, Link} from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import {FaCalendarAlt, FaDoorOpen, FaUsers} from "react-icons/fa";
 import UserPicker from "./component/Users/UserPicker.jsx";
+import BookingsPage from "./component/Bookings/BookingsPage.jsx";
+import BookablePage from "./component/Bookables/BookablePage.jsx";
+import UsersPage from "./component/Users/UsersPage.jsx";
 
 function App() {
 
@@ -34,7 +37,13 @@ function App() {
           <UserPicker/>
         </header>
       </div>
+      <Routes>
+        <Route path="/bookings" element={<BookingsPage/>}/>
+        <Route path="/bookables" element={<BookablePage/>}/>
+        <Route path="/users" element={<UsersPage/>}/>
+      </Routes>
     </BrowserRouter>
+    /* 사용자가 선택한 메뉴 항목에 따라 화면에 보이는 UI 결정 */
   )
 }
 
