95743e8d986698c6f95ff95039e7f7b91f1c4b62
SJC-s <sssm4719@gmail.com>
2024-10-07 14:53:47 +0900

    #15 setUserIndex 정리 전

--- src/component/Users/UserList.jsx
+++ src/component/Users/UserList.jsx
@@ -1,22 +1,26 @@
 import {useEffect, useState} from "react";
 import PageSpinner from "../UI/PageSpinner.jsx";
 
-function UserList() {
-    const [users, setUsers] = useState(null);
+function UserList({user, setUser}) {
+    const [users, setUsers] = useState(null); //  순서4) fetch 결과 상태값 저장
     // fetch 중 오류 발생시 또는 로딩 중의 상태값
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
     const [userIndex, setUserIndex] = useState(0);
-    const user = users?.[userIndex]; // 자바의 Optional 역할 연산자 : ?.(null 아닐 때만 실행)
+    // user = users?.[userIndex]; // 자바의 Optional 역할 연산자 : ?.(null 아닐 때만 실행)    // 순서5) users 0번을 user에 저장
+    // users 가 null 이 아닐 때만 실행
+
+    // 선택된 인덱스값으로 user 를 바꿔야 함(부모, 형제 컴포넌트와 공유할 수 있는 방법으로 바꿈)
+    setUser(users?.[userIndex])
 
     // API 서비스 제공하는 서버로부터 데이터 가져오기
     useEffect(() => {
         setLoading(true);
-        fetch("http://localhost:3002/users").then(response => {
+        fetch("http://localhost:3002/users").then(response => { // 순서 1)
             return response.json();
-        }).then(data => {
+        }).then(data => {  // 순서2) users 배열이 data로 저장
             console.log(data);
-            setUsers(data);
+            setUsers(data); // 순서3) 상태 users 변경
             setLoading(false)
         }).catch(error => {setError(error.message);})
     }, []);
@@ -31,6 +35,7 @@ function UserList() {
         return <PageSpinner/>
     }
 
+    // 순서 6) 컴포넌트를 가지고 UI를 만듦
     return (
         <>
             {users && (<ul className="users items-list-nav">
@@ -42,7 +47,9 @@ function UserList() {
                     </li>
                 ))}
             </ul>)}
-            {user && (<div className="item user">
+            {/*<UserDetails user={user}/>*/}
+            {/* UserDetails 로 컴포넌트 분리 */}
+    {/*        {user && (<div className="item user">
                 <div className="item-header">
                     <h2>{user.name}</h2>
                 </div>
@@ -50,7 +57,7 @@ function UserList() {
                     <h3>{user.title}</h3>
                     <p>{user.notes}</p>
                 </div>
-            </div>)}
+            </div>)}*/}
         </>
     )
 
--- src/component/Users/UsersPage.jsx
+++ src/component/Users/UsersPage.jsx
@@ -1,10 +1,15 @@
-import UserList from "./UserList.jsx";
+import UsersList from "./UserList.jsx";
+import {useContext, useState} from "react";
+import UserDetails from "./UserDetails.jsx";
 
-function UsersPage() {
+function UsersPage () {
+    const [user, setUser] = useState(null);
     return (
         <main className="users-page">
-            {/* 사용자 이름 목록 만들기 */}
-            <UserList/>
+            {/* 선택한 user 를 형제 컴포넌트와 공유하기 위해
+            부모 컴포넌트 UsersPage 가 user 상태를 선언했으므로 UserList 도 props 를 선언 */}
+            <UsersList user={user} setUser={setUser}/>
+            <UserDetails user={user}/>
         </main>
     )
 }
