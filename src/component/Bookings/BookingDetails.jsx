fd9244f71fabc4e473545800d122cf8c7e08d9a9
SJC-s <sssm4719@gmail.com>
2024-10-10 14:49:39 +0900

    #18 useSearchParam hook  Booking 리팩토링, BookingDetails 구현

--- src/component/Bookings/BookingDetails.jsx
+++ src/component/Bookings/BookingDetails.jsx
@@ -1,7 +1,41 @@
+import {useContext} from "react";
+import UserContext from "../Users/UserContext.js";
+import {FaEdit} from "react-icons/fa";
+import {formatDateDay} from "../utils/date-utils.js";
+
 export default function BookingDetails({booking, bookable}) {
+    // 현재 picker 선택한 사용자 가져오기
+    const {user, setUser} = useContext(UserContext);
+    // 현재 picker 선택한 사용자와 예약자가 같은지 판단
+    const isBooker = booking && user && (booking.bookerId === parseInt(user.id, 10))
+
+    // booking 객체 구조 분해하여 변수 설정, Grid 에서 한 칸을 선택해야 booking 객체가 만들어짐 -> undefined 값은 구조 분해 오류 발생
+    // const {title, date, session, notes} = booking
     return (
         <div className="booking-details">
-            <h3>예약 상세보기</h3>
+            <h2>예약 상세보기 {isBooker && (<span className="controls">
+                <button className="btn">
+                    <FaEdit/>
+                </button>
+            </span> )}</h2>
+            {booking ? (
+                <div className="booking-details-fields">
+                    <label>Title</label>
+                    <p>{booking.title}</p>
+                    <label>예약자원</label>
+                    <p>{bookable.title}</p> {/* bookerableId 값만 booking 에 저장 */}
+                    <label>예약 날짜</label>
+                    <p>{formatDateDay(new Date(booking.date))}</p>
+                    <label>Session</label>
+                    <p>{booking.session}</p>
+                    {booking.notes && (
+                        <>
+                            <label>Notes</label>
+                            <p>{booking.notes}</p>
+                        </>
+                    )}
+                </div>
+            ):(<div className="booking-details-fields">Select a booking or a booking slot</div>)}
         </div>
     )
 }
--- src/component/Bookings/BookingsPage.jsx
+++ src/component/Bookings/BookingsPage.jsx
@@ -2,13 +2,40 @@ import WeekPicker from "./WeekPicker.jsx";
 import BookablesList from "../Bookables/BookablesList.jsx";
 import {useState} from "react";
 import Bookings from "./Bookings.jsx";
+import useFetch from "../utils/useFetch.js";
+import {useSearchParams} from "react-router-dom";
+import PageSpinner from "../UI/PageSpinner.jsx";
 
 function BookingsPage() {
-    const [bookable, setBookable] = useState(null);
+    // 2) fetch 로 직접 해당 자원 조회하기 -> BookablesList 컴포넌트에게 전달
+    const {data:bookables = [], status, error} = useFetch("http://localhost:3002/bookables");
+
+    // 3) /bookings?bookableId=3 과 같이 쿼리스트링값을 가져오기
+    // url 에 실려오는 쿼리문자열을 가져오기 위해 2개의 값을 배열로 리턴해 줌
+    // searchParam(첫번째 리턴 값) : 함수 - 이 함수로 파라미터를 가져옴
+    // setSearchParam(두번째 리턴 값) : 첫번째 함수를 변경할 때 사용(우리는 사용 안함)
+    const [searchParam, setSearchParam] = useSearchParams()
+    // 4) url 에 포함된 쿼리 스트링 파라미터 가져오기
+    const id = searchParam.get("bookableId") // 파라미터 이름 알려주면 값을 전달
+
+    // 5) 조회한 bookable 저장, 파라미터가 없거나 조회결과 없으면 인덱스 0번값 저장
+    // id : 문자열 타입, 정수로 변환 => 비교 => 같은 id의 bookable 찾아 리턴
+    // || : or 연산을 활용한 단축 평가(|| 앞의 결과가 거짓이면 || 뒤의 식을 수행)
+    const bookable = bookables.find(b => b.id === parseInt(id,10)) || bookables[0];
+
+    if(status === "error") {
+        return <p>{error.message}</p>
+    }
+
+    if(status === "loading") {
+        return <PageSpinner/>
+    }
+
     return (
         <main className="bookings-page">
-            <BookablesList bookable={bookable} setBookable={setBookable}/>
-            {/* BookablesList 선택한  bookable 객체는 부모, 형제(Bookings)와 공유 */}
+            {/* 1) BookablesList 리팩토링 된 것에 맞춰 변경 */}
+            <BookablesList bookable={bookable} bookables={bookables} getUrl={(id) => `/bookings?bookableId=${id}`}/>
+            {/* BookablesList 선택한  bookable 객체는 Bookings 에게 프로퍼티값 단순한 전달 */}
             <Bookings bookable={bookable}/>
         </main>
     )
