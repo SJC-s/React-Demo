f6547a530d737280004ce2cf0236cc452c0b214c
SJC-s <sssm4719@gmail.com>
2024-10-11 10:20:02 +0900

    #22 useQuery bookables

--- src/component/Bookings/BookingsPage.jsx
+++ src/component/Bookings/BookingsPage.jsx
@@ -5,11 +5,14 @@ import Bookings from "./Bookings.jsx";
 import useFetch from "../utils/useFetch.js";
 import {useSearchParams} from "react-router-dom";
 import PageSpinner from "../UI/PageSpinner.jsx";
+import {useQuery} from "react-query";
+import loadData from "../utils/api.js";
 
 function BookingsPage() {
-    // 2) fetch 로 직접 해당 자원 조회하기 -> BookablesList 컴포넌트에게 전달
-    const {data:bookables = [], status, error} = useFetch("http://localhost:3002/bookables");
-
+    const {data: bookables = [], status, error} = useQuery(
+        "bookables",
+        () => loadData("http://localhost:3002/bookables")
+    );
     // 3) /bookings?bookableId=3 과 같이 쿼리스트링값을 가져오기
     // url 에 실려오는 쿼리문자열을 가져오기 위해 2개의 값을 배열로 리턴해 줌
     // searchParam(첫번째 리턴 값) : 함수 - 이 함수로 파라미터를 가져옴
